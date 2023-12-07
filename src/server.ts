53a4a18574a0125d87b83f185c80af8c3ac2db1e
iahmedelbayaa <elbayaaahmed845@gmail.com>
2023-12-07 17:38:41 +0200

    setup-file-structure

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,18 @@
-console.log("hello");
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+var app = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello Be3o!")
+})
+
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+})
